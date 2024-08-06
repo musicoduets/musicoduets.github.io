87beefc634eb52fa5eb61cf47ccc49afe0f3cecc
Aimee Co <aimeekco@gmail.com>
2024-08-06 13:09:06 -0700

    change layout

--- musico_app/src/components/Header.js
+++ musico_app/src/components/Header.js
@@ -1,19 +1,18 @@
 import React from "react";
-import { Link } from "react-router-dom";
 import "../styles/header.css";
 
 function Header() {
   return (
     <nav className="header">
       <div className="header-container">
-        <Link to="/" className="header-logo">
+        <a href="/" className="header-logo">
             musico
-        </Link>
+        </a>
         <div className="header-links">
-            <Link to="/about">About Us</Link>
-            <Link to="/services">Services</Link>
-            <Link to="/booking">Booking</Link>
-            <Link to="/contact">Contact</Link>
+            <a href="#about">About Us</a>
+            <a href="#services">Services</a>
+            <a href="#booking">Booking</a>
+            <a href="#contact">Contact</a>
         </div>
       </div>
     </nav>
--- musico_app/src/components/Home.js
+++ musico_app/src/components/Home.js
@@ -4,9 +4,43 @@ import "../styles/home.css";
 function Home() {
     return (
         <div className="home">
-            <h1>
-                Welcome to musico
-            </h1>
+            <section id="home">
+                <h1>
+                    Welcome to musico
+                </h1>
+            </section>
+            <section id="about">
+                <h2>
+                    About Us
+                </h2>
+                <p>
+                    lalalala
+                </p>
+            </section>
+            <section id="services">
+                <h2>
+                    Services
+                </h2>
+                <p>
+                    lalalala
+                </p>
+            </section>
+            <section id="booking">
+                <h2>
+                    Booking
+                </h2>
+                <p>
+                    lalalala
+                </p>
+            </section>
+            <section id="contact">
+                <h2>
+                    Contact
+                </h2>
+                <p>
+                    lalalala
+                </p>
+            </section>
         </div>
     );
 }
